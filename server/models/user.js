2bee8f9b4f9f6c40c0b93a948b23ed7048758853
jaime-lynn <jaime.madsen@gmail.com>
2017-03-02 10:45:42 -0700

    finished user model v1

--- server/models/user.js
+++ server/models/user.js
@@ -10,8 +10,38 @@ let schema = new Schema({
     username: { type: String, required: true, unique: true, uniqueCaseInsensitive: true },
     password: { type: String, required: true },
     steamdId: { type: String },
+    invites: [{ type: Object }],
     // Relations
-    friends: [{ type: ObjectId, ref: models.user }],
-    invites: [{ type: Object }]
-    
+    friends: [{ type: ObjectId, ref: models.user.name }],
+    groups: [{ type: ObjectId, ref: models.group.name }]
 })
+
+schema.pre('save', function(next){
+    var user = this;
+    if(!user.isModified('password')){
+        return next();
+    }
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt){
+        if(err){
+            return next(err);
+        } else {
+            bcrypt.hash(user.password, salt, function(err, hash){
+                user.password = hash;
+                next();
+            });
+        }
+    });
+});
+
+schema.methods.validatePassword = function(password){
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, this.password, function(err, isMatch){
+            if(err || !isMatch){
+                return reject(err);
+            }
+            return resolve(isMatch);
+        });
+    })
+}
+
+module.exports = mongoose.model(models.user.name, schema)
