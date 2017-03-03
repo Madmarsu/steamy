a611e3a39f124d5807479fc76f660e0c263559ad
TheEMP <gunnarDebate@gmail.com>
2017-03-03 13:54:02 -0700

    Start on front end

--- www/src/router/index.js
+++ www/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import ProfileRoutes from './profile.js'
 
 Vue.use(Router)
 
@@ -10,6 +11,7 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
-    }
+    },
+    ...ProfileRoutes
   ]
 })
