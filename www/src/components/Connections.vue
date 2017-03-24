a490bc692ca5186611ee7da6dc9b4ed6c758f99a
Jake Overall <joverall22@gmail.com>
2017-03-24 16:51:52 -0600

    Fixed responsiveness on connections

--- www/src/components/Connections.vue
+++ www/src/components/Connections.vue
@@ -22,7 +22,7 @@
                     </div>
                 </div>
 
-                <div class="col s6">
+                <div class="col s12 m7">
                     <div class="card blue-grey">
                         <div class="card-content white-text">
                             <h5 class="center">Friends</h5>
@@ -45,7 +45,7 @@
                     </div>
                 </div>
 
-                <div class="col s6">
+                <div class="col s12 m5">
                     <div class="card blue-grey">
                         <div class="card-content white-text">
                             <h5 class="center">Groups</h5>
